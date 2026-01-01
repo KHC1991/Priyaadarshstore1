a10a2df739bfb318d9be443b36f9d3fa0ea7a0c5
KHC1991 <netbankingappinfo@gmail.com>
2026-01-01 21:43:07 +0530

    Integrate Rating component into LandingPage
    Added Rating component to LandingPage route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
 import React, { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Intro from './components/Intro';
 import LandingPage from './components/LandingPage';
 import Auth from './components/Auth';
 import Dashboard from './components/Dashboard';
+import Rating from './Rating'; // અહીં આપણે રેટિંગ સિસ્ટમ ઉમેરી
 import { User, Language } from './types';
 
 const AppContent = () => {
@@ -27,10 +27,8 @@ const AppContent = () => {
         const ref = params.get('ref');
         
         if (ref) {
-          // If user comes from invite link, send to register
           navigate('/register' + location.search);
         } else if (location.pathname === '/') {
-          // If direct search, stay on landing page
           navigate('/');
         }
       }, 800); 
@@ -48,7 +46,15 @@ const AppContent = () => {
       
       <div className={showIntro ? 'hidden' : 'block'}>
         <Routes>
-          <Route path="/" element={<LandingPage language={language} setLanguage={setLanguage} />} />
+          <Route path="/" element={
+            <>
+              <LandingPage language={language} setLanguage={setLanguage} />
+              {/* લેન્ડિંગ પેજની નીચે રેટિંગ બતાવશે */}
+              <div style={{ maxWidth: '600px', margin: '0 auto', padding: '0 20px' }}>
+                <Rating />
+              </div>
+            </>
+          } />
           <Route path="/login" element={<Auth mode="login" language={language} setLanguage={setLanguage} onAuth={setUser} />} />
           <Route path="/register" element={<Auth mode="register" language={language} setLanguage={setLanguage} onAuth={setUser} />} />
           <Route path="/dashboard/*" element={user ? <Dashboard user={user} setUser={setUser} language={language} setLanguage={setLanguage} /> : <Auth mode="login" language={language} setLanguage={setLanguage} onAuth={setUser} />} />
